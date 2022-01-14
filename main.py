7c2775672ce5c074ba910e94c24ec4713e48eae4
Guilherme B <38567020+gui1080@users.noreply.github.com>
2022-01-14 16:05:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import itertools, random
 
+#-------------------------------- 1 --------------------------------
+
 Muldrotha_deck = [('Land', 'Swamp'), ('Land', 'Swamp'), ('Land', 'Swamp'), ('Land', 'Swamp'), ('Land', 'Swamp'), ('Land', 'Swamp'),
         ('Land', 'Forest'), ('Land', 'Forest'), ('Land', 'Forest'), ('Land', 'Forest'), ('Land', 'Forest'),
         ('Land', 'Island'), ('Land', 'Island'), ('Land', 'Island'), ('Land', 'Island'), ('Land', 'Island'), ('Land', 'Island'), ('Land', 'Island'),
@@ -66,3 +68,32 @@ prob7 = seven_land_hand/N
 
 
 # print(Muldrotha_deck)
+
+
+#-------------------------------- 2 --------------------------------
+
+
+#-------------------------------- 3 --------------------------------
+
+# PM -> Produces Mana
+# LPM -> Land that Produces Mana
+# CM -> Costs Mana
+
+Muldrotha_deck = [('LPM', 'Swamp'), ('LPM', 'Swamp'), ('LPM', 'Swamp'), ('LPM', 'Swamp'), ('LPM', 'Swamp'), ('LPM', 'Swamp'),
+        ('LPM', 'Forest'), ('LPM', 'Forest'), ('LPM', 'Forest'), ('LPM', 'Forest'), ('LPM', 'Forest'),
+        ('LPM', 'Island'), ('LPM', 'Island'), ('LPM', 'Island'), ('LPM', 'Island'), ('LPM', 'Island'), ('LPM', 'Island'), ('LPM', 'Island'),
+        ('LPM', 'Golgari Rot Farm'), ('LPM', 'Tainted Wood'), ('LPM', 'Tainted Isle'), ('LPM', 'Cabal Pit'), ('LPM', 'Bojuka Bog'), 
+        ('LPM', 'Exotic Orchard'), ('LPM', 'Hinterland Harbor'), ('LPM', 'Ipnu Rivulet'), ('LPM', 'Llanowar Wastes'),
+        ('LPM', 'Underground River'), ('LPM', 'Evolving Wilds'), ('LPM', 'Command Tower'), ('LPM', 'Sunken Hollow'), ('LPM', 'Terramorphic Expanse'),
+        ('CM', 'Perpetual Timepiece'), ('CM', 'Nevinyrrals Disk'), ('PM', 'Lotus Petal'), ('PM', 'Sol Ring'), ('PM', 'Birds of Paradise'), ('CM', 'Altar of Dementia'),
+        ('PM', 'Possessed Portal'), ('PM', 'Jesters Mask'), ('PM', 'Chromatic Lantern'), ('CM', 'Demonic Consultation'), ('CM', 'Dispel'),
+        ('CM', 'Beseech the Queen'), ('CM', 'Counterspell'), ('CM', 'Negate'), ('PM', 'Kodamas Reach'), ('PM', 'Cultivate'), ('CM', 'Buried Alive'),
+        ('CM', 'Fact or Fiction'), ('CM', 'Diabolic Tutor'), ('CM', 'Diabolic Revelation'), ('CM', 'Siren Stormtamer'), 
+        ('PM', 'Satyr Wayfinder'), ('PM', 'Priest of Titania'), ('PM', 'Fyndhorn Elves'), ('PM', 'Elves of Deep Shadow'), ('CM', 'Mirror-Mad Phantasm'), ('CM', 'Coiling Oracle'), ('CM', 'Thassas Oracle'), ('CM', 'Gravebreaker Lamia'),
+        ('CM', 'Stitchers Supplier'), ('PM', 'Llanowar Elves'), ('PM', 'Elvish Mystic'), ('CM', 'Spore Frog'), ('PM', 'Sakura-Tribe Elder'), ('CM', 'Divining Witch'),
+        ('CM', 'Lord of Extinction'), ('CM', 'Mikaeus, the Unhallowed'), ('CM', 'Tidespout Tyrant'), ('CM', 'Sidisi, Undead Vizier'), ('CM', 'Doom Whisperer'), ('PM', 'Eternal Witness'), ('CM', 'Jarad, Golgari Lich Lord'), ('CM', 'Tatyova, Benthic Druid'),
+        ('CM', 'Deathrite Shaman'), ('CM', 'World Shaper'), ('CM', 'Dreamborn Muse'), ('PM', 'Ilysian Caryatid'), ('CM', 'Jace, Wielder of Mysteries'),
+        ('CM', 'Kiora, Master of the Depths'), ('CM', 'Kayas Ghostform'), ('CM', 'Seal of Removal'), ('CM', 'Chronic Flooding'), ('CM', 'Sultai Ascendancy'), ('PM', 'Khalni Heart Expedition'), 
+        ('CM', 'Animate Dead'), ('CM', 'Pernicious Deed'), ('CM', 'Rhystic Study'), ('CM', 'Second Chance'), ('PM', 'Defense of the Heart'),
+        ('CM', 'Phyrexian Scriptures'), ('CM', 'Zurs Weirding')]
+
